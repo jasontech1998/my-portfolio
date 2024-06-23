df8f1b0b9beced12e16602da4e934f464d18cfd9
Jason Yu <jasontech1998@gmail.com>
2024-06-23 14:33:46 -0700

    fix custom nav bar component styling

--- components/creative/CustomNav.tsx
+++ components/creative/CustomNav.tsx
@@ -11,13 +11,13 @@ export default function CustomNav() {
   return (
     <nav
       onMouseLeave={() => setFocused(null)}
-      className="relative z-0 max-w-max flex-1 items-center justify-center pt-2"
+      className="relative z-0 w-full flex-1 items-center justify-center pt-2"
     >
       <ul className="group flex flex-1 list-none items-center justify-center">
         {items.map((item) => (
           <li
             onMouseEnter={() => setFocused(item)}
-            className="relative cursor-pointer flex justify-center items-center w-20 h-10 mx-3 px-0"
+            className="relative cursor-pointer flex justify-center items-center p-2 mx-2"
             key={item}
           >
             <span className="relative z-0 font-medium">{item}</span>
@@ -27,7 +27,7 @@ export default function CustomNav() {
                   duration: 0.3,
                   type: "spring",
                 }}
-                className="absolute rounded-lg -z-10 bg-gray-100 w-20 h-10"
+                className="absolute inset-0 -z-10 p-3 w-full rounded-md bg-gray-100"
                 style={{ bottom: "0px", left: "0px", width: "100%" }}
                 layoutId="highlight"
               />
