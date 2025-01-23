d04b7d26f4dcfb36950e06d8fd3351b43c9c1522
Jason Yu <jasontech1998@gmail.com>
2025-01-23 15:18:36 -0800

    small bio update

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -2,7 +2,7 @@ export function Intro() {
   return (
     <section>
       <p>
-        Hi, I&apos;m a front end engineer based in Bay Area, CA.
+        Hi, I&apos;m a software engineer based in Bay Area, CA.
       </p>
     </section>
   );
